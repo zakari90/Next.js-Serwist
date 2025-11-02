6114195fd2b4c415c9f670d8f50f254260d5a6fc
zakari90 <zakariazinedine1@gmail.com>
2025-11-02 22:00:46 +0100

    test 2

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,16 @@
+"use client"
 import React from 'react'
-
+import Testc from '../../pages/testc'
+import { useState } from 'react';
 function Page() {
+    const [first, setfirst] = useState<number>(0)
+
   return (
-    <div>bismi lah</div>
+    <div>bismi lah
+        <button onClick={()=>setfirst(first+1)}>click me</button>
+        <Testc id={first}
+        />
+    </div>
   )
 }
 
---
+++ pages/testc.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Testc({id}: {id?: number}) {
+  return (
+    <div>Testc {id}</div>
+  )
+}
+
+export default Testc
