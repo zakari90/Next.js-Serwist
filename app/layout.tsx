ed5e0a0c9799560414a1860c9dc0124a1d0d61c3
zakari90 <zakariazinedine1@gmail.com>
2025-11-02 17:52:07 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import type { ReactNode } from "react";
+import ServiceWorkerRegister from "../pages/client-sw-register";
 
 const APP_NAME = "NJS App";
 const APP_DESCRIPTION = "Next.js + Serwist PWA";
@@ -46,7 +47,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             }
             `}</style>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <ServiceWorkerRegister/>
+        
+        </body>
     </html>
   );
 }
---
+++ pages/client-sw-register.tsx
@@ -0,0 +1,14 @@
+// app/client-sw-register.tsx
+"use client";
+import { useEffect } from "react";
+
+export default function ServiceWorkerRegister() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js");
+      });
+    }
+  }, []);
+  return null;
+}
